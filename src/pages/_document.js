7f91d095a4332357a1275a6735c77cbe2181c31c
Nitin_host <nitinravella02@gmail.com>
2025-03-28 00:16:46 +0530

    contact page changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,16 +5,9 @@ class MyDocument extends Document {
         return (
           <Html lang='en'>
             <Head>
-              <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link
-                rel="preconnect"
-                href="https://fonts.gstatic.com"
-                crossOrigin="anonymous"
-              />
-              <link
-                href="https://fonts.googleapis.com/css2?family=Prata&display=swap"
-                rel="stylesheet"
-              />
+              <link rel="preconnect" href="https://fonts.googleapis.com"/>
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+              <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
               <link rel="icon" href="/favicon.ico" />
             </Head>
             <body>
